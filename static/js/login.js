363a60c87c6eee92ba947078cdc82f46ca808ead
Tar-Tarus <loubia314@gmail.com>
2017-04-21 13:40:08 +0200

    Added functioning login modal  \(*_*)/

--- static/js/login.js
+++ static/js/login.js
@@ -9,13 +9,22 @@ $(document).ready(function(){
 	    alert("Please fill all fields!");
 	}else {
 	    amivcore.login(user, password, function(loginReturn){
-		if (loginReturn !== true)
-		    alert("Wrong credentials!")
+		if (loginReturn !== true){
+		    alert("Wrong credentials!");
+                } else {
+                    console.log("Success! Loged in");
+                    $("#LoginFormNavbar").attr("style","display:none;");
+                    $("#LogoutFormNavbar").removeAttr("style");
+                    //$("#LoginModal").modal('hide');
+                }
 	    })
 	}
     });
-    //$("#LoginModal").modal('toggle');
-    //$("#loginHeader").leanModal({
+    $("#LogoutButtonNavbar").click(function(){
+        $("#LogoutFormNavbar").attr("style","display:none;");
+        $("#LoginFormNavbar").removeAttr("style");
+    });
+        //$("#loginHeader").leanModal({
 //	top: 100,
 //	overlay: 0.6,
 //	closeButton: ".modal_close"
